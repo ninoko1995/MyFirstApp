f5d61cab2317777ae0494e47ec05f73c49b6d395
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-09-10 21:59:35 +0900

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Kota Ninomiya on 2019/09/10.
+//  Copyright © 2019 Kota Ninomiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
