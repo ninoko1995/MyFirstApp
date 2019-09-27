c4890778a9e67d7dc4ff489d10291287d7334b27
ninoko1995 <ninoninomiyamiya1995@gmail.com>
2019-09-27 23:13:21 +0900

    set rooting

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -32,16 +32,20 @@ class ViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // 遷移先のDetailViewControllerを取得
-        let detailViewController = segue.destination as! DetailedViewController
-        // タップされたボタン要素を取得
-        let tappedButton = sender as! UIButton;
-        // ボタンのタグを取得
-        let buttonTag = tappedButton.tag
-        // 詳細画面に表示する画像名を生成
-        let detailImageName = "image" + String(buttonTag)
-        // 詳細画面に画像名を渡す
-        detailViewController.imageName = detailImageName
+        if segue.destination is DetailedViewController {
+            // 遷移先のDetailViewControllerを取得
+            let detailViewController = segue.destination as! DetailedViewController
+            // タップされたボタン要素を取得
+            let tappedButton = sender as! UIButton;
+            // ボタンのタグを取得
+            let buttonTag = tappedButton.tag
+            // 詳細画面に表示する画像名を生成
+            let detailImageName = "image" + String(buttonTag)
+            // 詳細画面に画像名を渡す
+            detailViewController.imageName = detailImageName
+        }
+        if segue.destination is IndexViewController {
+        }
     }
 }
 
